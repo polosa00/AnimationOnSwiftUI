959c63f8779739bacb5d7b72b3303b40e30aa93d
polosa00 <i@apolochanin.ru>
2023-06-26 15:06:24 +0300

    Create Heart-Beat Animation

--- AnimationOnSwiftUI/ContentView.swift
+++ AnimationOnSwiftUI/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showRate = false
+    @State private var heartBeat = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            Image("heart")
+                .resizable()
+                .frame(width: 150, height: 150)
+                .scaleEffect(heartBeat ? 1 : 1.3)
+                .animation(.interpolatingSpring(stiffness: 30, damping: 10)
+                    .speed(1.3/2)
+                    .repeatForever(autoreverses: false),
+                           value: heartBeat
+                )
+                .onAppear() {
+                    heartBeat.toggle()
+                }
+            Image("heart-beat")
+                .resizable()
+                .frame(width: 130, height: 100)
+                .clipShape(Rectangle().offset(x: showRate ? 0 : -130))
+                .animation(.interpolatingSpring(stiffness: 30, damping: 10)
+                    .speed(1.3)
+                    .repeatForever(autoreverses: true)
+                    .delay(0.2),
+                           value: showRate
+                )
+                .offset(x:-5, y: 10)
+                .onAppear() {
+                    showRate.toggle()
+                }
         }
-        .padding()
+        .shadow(color: .red, radius: 8)
     }
 }
 
