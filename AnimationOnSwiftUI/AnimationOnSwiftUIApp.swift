21d9bcdd8551f47f286e846d1bb7aeda8564bb14
polosa00 <i@apolochanin.ru>
2023-06-26 11:00:31 +0300

    Initial Commit

---
+++ AnimationOnSwiftUI/AnimationOnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationOnSwiftUIApp.swift
+//  AnimationOnSwiftUI
+//
+//  Created by Александр Полочанин on 26.06.23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationOnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
